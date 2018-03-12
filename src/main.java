61e51cf8db5202e89eff2b42abb3ceb29910e78d
Janosch Diggelmann <janosch.digg@outlook.com>
2018-03-12 10:38:18 +0100

    HANGMAN

--- src/main.java
+++ src/main.java
@@ -2,8 +2,9 @@
 
 public class main 
 {	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		Hangman hang = new Hangman();
 		System.out.println("OK COOL");
 	}
 }
