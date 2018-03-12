351800dc559ecf4089b6791f1c78ce949231bdf3
Janosch Diggelmann <janosch.digg@outlook.com>
2018-03-12 10:08:52 +0100

    HANGMAN

--- src/main.java
+++ src/main.java
@@ -3,6 +3,54 @@ public class main
 {	
 	public static void main(String[] args) 
 	{
-		System.out.println("OK COOL");
+		System.out.println("  +---+\r\n" + 
+				"  |   |\r\n" + 
+				"      |\r\n" + 
+				"      |\r\n" + 
+				"      |\r\n" + 
+				"      |\r\n" + 
+				"=========");
+		System.out.println("  +---+\r\n" + 
+				"  |   |\r\n" + 
+				"  O   |\r\n" + 
+				"      |\r\n" + 
+				"      |\r\n" + 
+				"      |\r\n" + 
+				"=========");
+		System.out.println("  +---+\r\n" + 
+				"  |   |\r\n" + 
+				"  O   |\r\n" + 
+				"  |   |\r\n" + 
+				"      |\r\n" + 
+				"      |\r\n" + 
+				"=========");
+		System.out.println("  +---+\r\n" + 
+				"  |   |\r\n" + 
+				"  O   |\r\n" + 
+				" /|   |\r\n" + 
+				"      |\r\n" + 
+				"      |\r\n" + 
+				"=========");
+		System.out.println("  +---+\r\n" + 
+				"  |   |\r\n" + 
+				"  O   |\r\n" + 
+				" /|\\  |\r\n" + 
+				"      |\r\n" + 
+				"      |\r\n" + 
+				"=========");
+		System.out.println("  +---+\r\n" + 
+				"  |   |\r\n" + 
+				"  O   |\r\n" + 
+				" /|\\  |\r\n" + 
+				" /    |\r\n" + 
+				"      |\r\n" + 
+				"=========");
+		System.out.println("  +---+\r\n" + 
+				"  |   |\r\n" + 
+				"  O   |\r\n" + 
+				" /|\\  |\r\n" + 
+				" / \\  |\r\n" + 
+				"      |\r\n" + 
+				"=========");
 	}
 }
