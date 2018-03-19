c74b38c8e51e38edcbd8616c7091c453a3f2e89d
DenisTurkovic <d.turkovic00@gmx.ch>
2018-03-19 10:56:58 +0100

    test

--- src/main.java
+++ src/main.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 public class main 
 {	
@@ -15,7 +15,27 @@ public class main
 //		}
 		
 		// Stephanie
-		System.out.println("======================== STEPHANIE ========================");
-		BenzinRechner benzinRechner = new BenzinRechner();
+		//System.out.println("======================== STEPHANIE ========================");
+		//BenzinRechner benzinRechner = new BenzinRechner();
+		
+		
+		// Denis
+		Hangman hangman = new Hangman("Test");
+		hangman.guess('T');
+		hangman.guess('a');
+		hangman.guess('b');
+		hangman.guess('z');
+		hangman.guess('u');
+		hangman.guess('i');
+		hangman.guess('k');
+		hangman.guess('l');
+		hangman.guess('o');
+		
+		Hangman hangman2 = new Hangman("Hello");
+		Scanner scan = new Scanner(System.in);
+		boolean check = true;
+		do {
+			check = hangman2.guess(scan.next().charAt(0));
+		} while (check);
 	}
 }
